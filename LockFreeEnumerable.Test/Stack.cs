ee9292d10702427610957ecc50ae83ee3e552bd0
S.Aprelov(HOME) <s.a.aprelov@gmail.com>
2013-07-14 20:38:31 +0400

    One thread stack

---
+++ LockFreeEnumerable.Test/Stack.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockFreeEnumerable.Test
+{
+    [TestClass]
+    public class Stack
+    {
+        [TestMethod]
+        public void CanCreate()
+        {
+            var stack = new LFStack<object>();
+            Assert.IsNotNull(stack);
+        }
+
+        [TestMethod]
+        public void CanPush()
+        {
+            var value = 1;
+            var stack = new LFStack<object>();
+            stack.Push(value);
+            Assert.AreEqual(value, stack.Pop());
+        }
+
+    }
+}
---
+++ LockFreeEnumerable/LFStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LockFreeEnumerable.Test
+{
+    public class LFStack<T1>
+    {
+        private Item<T1> head;
+
+        private struct Item<T2>
+        {
+            public T2 Value;
+            
+            public object Next;
+        }
+
+        public void Push(T1 value)
+        {
+            head = new Item<T1> { Value = value, Next = head };
+        }
+
+        public T1 Pop()
+        {
+            var pop = head;
+            head = (Item<T1>)pop.Next;
+            return pop.Value;
+        }
+    }
+}
