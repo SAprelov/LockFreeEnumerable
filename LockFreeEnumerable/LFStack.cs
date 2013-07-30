28bbcd8119f5614df842974f45a13fc08b5709c7
S.Aprelov(HOME) <s.a.aprelov@gmail.com>
2013-07-30 22:42:52 +0400

    Lock free stack added

--- LockFreeEnumerable/LFStack.cs
+++ LockFreeEnumerable/LFStack.cs
@@ -1,31 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 
-namespace LockFreeEnumerable.Test
+namespace LockFreeEnumerable
 {
     public class LFStack<T1>
     {
         private Item<T1> head;
 
-        private struct Item<T2>
-        {
-            public T2 Value;
-            
-            public object Next;
-        }
-
         public void Push(T1 value)
         {
-            head = new Item<T1> { Value = value, Next = head };
+            var item = new Item<T1>(value)
+            {
+                Next = head
+            };
+
+            while (item.Next != Interlocked.CompareExchange(ref head, item, item.Next))
+            {
+                item.Next = head;
+            }
         }
 
         public T1 Pop()
         {
-            var pop = head;
-            head = (Item<T1>)pop.Next;
-            return pop.Value;
+            if (head == null)
+            {
+                return default(T1);
+            }
+            
+            var item = head;
+            while (item != Interlocked.CompareExchange(ref head, item.Next, item))
+            {
+                item = head;
+            }
+            return item.Value;
+        }
+
+        private class Item<T2>
+        {
+            public Item<T2> Next;
+            public readonly T2 Value;
+
+            public Item(T2 value)
+            {
+                Value = value;
+            }
         }
     }
 }
